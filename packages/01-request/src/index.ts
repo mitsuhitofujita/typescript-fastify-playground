64b9435909977e2fba462fbec4e8837d3d6c92ec
Mitsuhito Fujita <mail@mitsuhito.dev>
2025-09-18 12:41:12 +0000

    feat: add post request

--- packages/01-request/src/index.ts
+++ packages/01-request/src/index.ts
@@ -6,6 +6,31 @@ server.get('/ping', async (_request, _reply) => {
   return 'pong\n'
 })
 
+const putSchema = {
+  params: {
+    type: 'object',
+    required: ['id'],
+    properties: {
+      id: { type: 'string' }
+    }
+  },
+  body: {
+    type: 'object',
+    required: ['title', 'body', 'author'],
+    properties: {
+      title: { type: 'string', maxLength: 100 },
+      body: { type: 'string', maxLength: 1000 },
+      author: { type: 'string', maxLength: 20 }
+    }
+  }
+}
+
+server.put('/posts/:id', { schema: putSchema }, async (request, _reply) => {
+  const { id } = request.params as { id: string }
+  const { title, body, author } = request.body as { title: string; body: string; author: string }
+  return { message: 'Post updated successfully', data: { id, title, body, author } }
+})
+
 server.listen({ port: 3000 }, (err, address) => {
   if (err) {
     console.error(err)
